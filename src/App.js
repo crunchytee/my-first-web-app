eecbe6832d132c11981c6b568cebcf64fcef8792
crunchytee <tosh.robertsbrockway@gmail.com>
2021-08-31 20:58:57 -0700

    added login elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Login />
     </Router>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,78 @@
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 
-const {
-  register,
-  handleSubmit,
-  watch,
-  formState: { errors },
-} = useForm({ mode: "onBlur" });
-const history = useHistory();
-const onSubmit = (data) => {
-  console.log(data);
-  history.push("/success");
-};
-
 const Login = () => {
+  const USERNAME = /admin/;
+  const PASWORD = /password/;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
+  const history = useHistory();
+  const onSubmit = (data) => {
+    console.log(data);
+    history.push("/");
+  };
   return (
     <div className="grid place-items-center h-screen w-screen">
       <form
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
         onSubmit={handleSubmit(onSubmit)}
-      ></form>
+      >
+        <div className="text-xl font-medium text-black p-6 block mx-36">
+          Log In
+        </div>
+        <label
+          htmlFor="username"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
+          Username
+          <input
+            id="username"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Username"
+            {...register("username", {
+              required: "Username required",
+              pattern: {
+                value: USERNAME,
+                message: "Invalid username. Try 'admin'",
+              },
+            })}
+          />
+          {errors["username"] && (
+            <p className="text-red-500">{errors["username"].message}</p>
+          )}
+        </label>
+        <label
+          htmlFor="password"
+          className="block text-gray-700 text-sm font-bold mb-2"
+        >
+          Password
+          <input
+            id="password"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            placeholder="Password"
+            {...register("password", {
+              required: "Password required",
+              pattern: {
+                value: PASWORD,
+                message: "Invalid password. Try 'password'",
+              },
+            })}
+          />
+          {errors["password"] && (
+            <p className="text-red-500">{errors["password"].message}</p>
+          )}
+        </label>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-36 my-4 rounded"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
