6c4c308572cfa42079548178f29c005aa5332dfc
crunchytee <tosh.robertsbrockway@gmail.com>
2021-09-01 21:10:57 -0700

    added welcome page & associated routes

---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <div className="grid place-items-center h-screen w-screen">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="block p-6">
+          This is the welcome page. Welcome to my page. I hope you like it here.{" "}
+          <a className="underline text-blue-600">
+            <Link to="/login">Log In</Link>
+          </a>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
