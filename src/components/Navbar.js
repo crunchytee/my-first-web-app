e6838cbadea9cd9318d5139689a041882638c012
crunchytee <tosh.robertsbrockway@gmail.com>
2021-09-06 14:02:09 -0700

    added responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row w-screen h-20">
-      <li className="m-6">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="m-6">
-        <Link to="/login">Log In</Link>
-      </li>
-    </div>
+    <nav className="flex flex-row w-screen h-20 bg-white border-b-2 border-gray-400 justify-between">
+      <div className="m-6 text-lg">myWebsite</div>
+      <ul>
+        <div className=" btn-header">
+          <Link to="/">Home</Link>
+        </div>
+        <div className=" btn-header">
+          <Link to="/login">Log In</Link>
+        </div>
+      </ul>
+    </nav>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: (theme) => ({
+      primary: "white",
+      header: "#00B2CA",
+    }),
     extend: {},
   },
   variants: {
