b266262db4e2faf728bcbf82e641bdc3ee1eafc5
crunchytee <tosh.robertsbrockway@gmail.com>
2022-02-12 12:04:16 -0800

    add login authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory, Link } from "react-router-dom";
 import LoginContext from "../LoginContext";
+import Modal from "react-modal/lib/components/Modal";
 
 const Login = () => {
   const USERNAME = /admin/;
   const PASWORD = /password/;
   const { authenticated, setAuthenticated } = useContext(LoginContext); // eslint-disable-line no-unused-vars
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("Nothing!");
+
+  // React hook form initiation
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ mode: "onBlur" });
+
+  // Setup history for submit
   const history = useHistory();
+
+  // Handle submit
   const onSubmit = async (data) => {
     const response = await fetch("http://127.0.0.1:5000/login", {
       method: "POST",
@@ -21,12 +30,27 @@ const Login = () => {
         "Content-Type": "application/json",
       },
     });
-    console.log(response.status);
+    console.log(`Response status: ${response.status}`);
 
-    //TODO: Implement response checking / conditions based on status code
-    setAuthenticated(true);
-    history.push("/dashboard");
+    // Reroute on success/unsuccess.
+    // This is great but it doesn't work when the request doesn't return a response at all (server not online)
+    if (response.status === 200) {
+      setAuthenticated(true);
+      history.push("/dashboard");
+    } else if (response.status === 401) {
+      history.push("/login");
+      setErrorMessage(
+        "That wasn't quite right. Are you who you say you are, or are you just forgetting your credentials?"
+      );
+      toggleModal();
+    } else {
+      setErrorMessage("Something went wrong on our end...");
+      toggleModal();
+    }
   };
+  function toggleModal() {
+    setIsModalOpen(!isModalOpen);
+  }
   return (
     <div className="md:w-fit-content md:mx-auto">
       <form
@@ -95,6 +119,23 @@ const Login = () => {
           </Link>
         </p>
       </form>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={toggleModal}
+        ariaHideApp={false}
+      >
+        <div className="w-fit-content mx-auto text-xl font-medium text-black p-6">
+          {errorMessage}
+        </div>
+        <div className="mx-auto md:w-fit-content">
+          <button
+            onClick={toggleModal}
+            className="bg-button w-100% hover:bg-buttonHover text-white font-bold py-2 px-4 my-4 rounded "
+          >
+            Try Again
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
