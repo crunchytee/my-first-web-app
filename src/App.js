9d71bf16bddf9dbf8a7947cc53a3b4584c3fee52
Forrest Chew <forrestchew@gmail.com>
2022-07-09 21:43:18 -0700

    new structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import Dashboard from './components/Dashboard';
-import Welcome from './components/Welcome';
-import ProtectedRoute from './components/ProtectedRoute';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+// import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <ProtectedRoute path="/dashboard" component={Dashboard} />
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <Welcome />
-        </Route>
+        {/* <ProtectedRoute /> */}
+        <Route path="/signup"></Route>
       </Switch>
     </Router>
   );
--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,21 +1,21 @@
-import React, { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
-import LoginContext from "../LoginContext";
+// import React, { useContext } from "react";
+// import { Route, Redirect } from "react-router-dom";
+// import LoginContext from "../LoginContext";
 
-function ProtectedRoute({ component: Component, ...rest }) {
-  const { authenticated } = useContext(LoginContext);
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        return authenticated ? (
-          <Component {...rest} {...props} />
-        ) : (
-          <Redirect to="/login" />
-        );
-      }}
-    />
-  );
-}
+// function ProtectedRoute({ component: Component, ...rest }) {
+//   const { authenticated } = useContext(LoginContext);
+//   return (
+//     <Route
+//       {...rest}
+//       render={(props) => {
+//         return authenticated ? (
+//           <Component {...rest} {...props} />
+//         ) : (
+//           <Redirect to="/login" />
+//         );
+//       }}
+//     />
+//   );
+// }
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
